5bd2157590ad15e7fa8866a33b2283113599f8ec
Katerina034 <blinovakater@yandex.ru>
2018-11-01 09:16:10 +0400

    Начало положено

---
+++ src/PO61/Blinova/wdad/learn/xml/XmlTask.java
@@ -0,0 +1,17 @@
+package PO61.Blinova.wdad.learn.xml;
+
+public class XmlTask {
+    public String getNoteText (User owner, String title)
+    {
+
+    }
+    public void updateNote (User owner, String title, String newText)
+    {
+
+    }
+    public void setPrivileges (String noteTitle, User user, int newRights)
+    {
+
+    }
+
+}
