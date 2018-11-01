5e1aec16aca8de918d40c0caa92e5b827337f0d2
Katerina034 <blinovakater@yandex.ru>
2018-11-01 08:46:02 +0400

    Added entry point

---
+++ src/PO61/Blinova/wdad/Application.java
@@ -0,0 +1,9 @@
+package PO61.Blinova.wdad;
+
+public class Application
+{
+    public static void main(String[] args)
+    {
+        System.out.print("I'm Ekaterina Blinova, and I'm not a monkey");
+    }
+}
