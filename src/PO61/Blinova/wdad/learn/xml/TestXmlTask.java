6db3391312ec6a0346917b214f5f238fe12d9766
Katerina034 <blinovakater@yandex.ru>
2018-11-01 10:47:03 +0400

    test is успешно

--- src/PO61/Blinova/wdad/learn/xml/TestXmlTask.java
+++ src/PO61/Blinova/wdad/learn/xml/TestXmlTask.java
@@ -1,8 +1,30 @@
 package PO61.Blinova.wdad.learn.xml;
 
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
 public class TestXmlTask {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        User owner = new User();
+        owner.setName("Имя");
+        owner.setMail("Почта");
+        owner.setRights("R");
+
+        XmlTask xmlTask = new XmlTask();
+        String answer = xmlTask.getNoteText(owner, "Заголовок" );
+        System.out.print(answer);
+
+        xmlTask.updateNote(owner,"Заголовок", "Новый текст");
+
+        User user = new User();
+        user.setName("Имя пользователя1");
+        user.setMail("Почта пользователя1");
+        user.setRights("R");
+
+
+        xmlTask.setPrivileges("Заголовок2",user,0);
 
     }
 }
