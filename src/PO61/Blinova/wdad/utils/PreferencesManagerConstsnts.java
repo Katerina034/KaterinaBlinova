5bb97f77fa3a5772357c20b81c52b99f42a59a40
Katerina034 <blinovakater@yandex.ru>
2018-12-13 09:36:36 +0400

    task4 не до конца

--- src/PO61/Blinova/wdad/utils/PreferencesManagerConstsnts.java
+++ src/PO61/Blinova/wdad/utils/PreferencesManagerConstsnts.java
@@ -2,9 +2,28 @@ package PO61.Blinova.wdad.utils;
 
 public interface PreferencesManagerConstsnts {
     public static final String CREATE_REGISTRY = "rmi.server.registry.createregistry";
+
     public static final String REGISTRY_ADDRESS = "rmi.server.registry.registryaddress";
+
     public static final String REGISTRY_PORT = "rmi.server.registry.registryport";
+
     public static final String POLICY_PATH = "rmi.client.policypath";
+
     public static final String USE_CODE_BASE_ONLY = "rmi.client.usecodebaseonly";
+
     public static final String CLASS_PROVIDER = "rmi.classprovider";
+
+    public static final String CLASS_NAME = "datasource.classname";
+
+    public static final String DRIVER_TYPE = "datasource.drivertype";
+
+    public static final String HOST_NAME = "datasource.hostName";
+
+    public static final String PORT = "datasource.port";
+
+    public static final String DB_NAME = "datasource.DBName";
+
+    public static final String USER = "datasource.user";
+
+    public static final String PASS = "datasource.pass";
 }
